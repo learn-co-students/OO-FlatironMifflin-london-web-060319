909aec7f1639325e1c930b3ca46a115180f6121a
Federica Macchi <federica.macchi3@gmail.com>
2019-06-09 21:16:15 +0100

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,38 @@
 class Employee
 
+    attr_reader :name, :salary
+    @@all = []
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def manager_name
+        my_manager = Manager.all.find do |manager|
+            manager.employees.include?(self)
+            return manager.name
+        end
+        return my_manager
+    end 
+
+    def self.paid_over(amount)
+        @@all.select {|employee| employee.salary > amount}
+        
+    end
+
+    def self.find_by_department(dept)
+        my_manager = Manager.all.find do |manager| 
+            manager.department == dept
+        end
+        return my_manager.employees[0]
+    end
+
+    def tax_bracket
+        @@all.select { |employee| employee.salary >= self.salary - 1000 && employee.salary <= self.salary + 1000 }
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Manager
+
+    attr_reader :name, :department, :age  
+    attr_accessor :employees
+    @@all = []
+    @@all_departments = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @employees = [] 
+        @@all << self
+        @@all_departments << department
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_departments
+        @@all_departments
+    end
+
+    def hire_employee(name, salary)
+        employee = Employee.new(name, salary)
+        @employees << employee
+        employee
+    end
+
+    def self.average_age
+        ages = self.all.map { |manager| manager.age}
+        return ages.reduce { |sum, age| sum + age}/@@all.length
+    end
 
 end
