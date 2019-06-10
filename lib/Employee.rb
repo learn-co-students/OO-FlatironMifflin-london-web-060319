dc9e7c1fbaeef92bb75600449ad59f82a8eb9b76
Pascal Rassaby <pasbynumbers@gmail.com>
2019-06-10 01:31:40 +0100

    Completed deliverables

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,36 @@
-class Employee
+# frozen_string_literal: true
 
+class Employee
+  attr_accessor(:name, :salary, :manager_name)
+  attr_reader
+  attr_writer
+
+  @@all = []
+
+  def initialize(name:, salary:, manager_name:)
+    @name = name
+    @salary = salary
+    @manager_name = manager_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.paid_over(integer)
+    @@all.select { |employee| employee.salary > integer }
+  end
+
+  def self.find_by_department(dept_search)
+    @@all.find do |employee|
+      Manager.all.find do |manager|
+        manager.department == dept_search && manager.name == employee.manager_name
+      end
+    end
+  end
+
+  def tax_bracket
+    @@all.select { |employee| employee.salary.between?(@salary - 1000, @salary + 1000) }
+  end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,14 @@ require 'pry'
 
 
 #Test your code here
-
+emp1 = Employee.new(name: "Jason", salary: 25000, manager_name: "Viola")
+emp6 = Employee.new(name: "Kimmy", salary: 25800, manager_name: "Viola")
+emp2 = Employee.new(name: "Lucy", salary: 27000, manager_name: "Pablo")
+emp3 = Employee.new(name: "Victor", salary: 32000, manager_name: "Viola")
+emp4 = Employee.new(name: "Logan", salary: 23400, manager_name: "Pablo")
+emp5 = Employee.new(name: "Daphne", salary: 30500, manager_name: "Pablo")
+man1 = Manager.new(name: "Viola", department: "Marketing", age: 34)
+man2 = Manager.new(name: "Pablo", department: "HR", age: 29)
 
 binding.pry
 puts "done"
