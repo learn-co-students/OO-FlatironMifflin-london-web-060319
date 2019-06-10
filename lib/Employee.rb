b74766fc791640994a895011848bb79f6034cfde
github username <github email address>
2019-06-10 16:13:38 +0100

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,29 @@
 class Employee
+    attr_accessor :name, :salary, :manager
+    @@all = []
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(amount)
+        all.select{|employee|employee.salary > amount}
+    end
+
+    def self.find_by_department(department)
+        all.find{|employee| employee.manager.department == department}
+    end
+
+    def tax_bracket
+        mySalary = self.salary
+       lower_salary = mySalary - 1000
+       higher_salary = mySalary + 1000
+       Employee.all.select {|employee| employee.salary >= lower_salary && employee.salary <= higher_salary} 
+    end
 end
--- run.rb
+++ run.rb
@@ -1,9 +1,18 @@
 require_relative "lib/Manager"
 require_relative "lib/Employee"
+# require_relative "lib/department"
 require 'pry'
 
 
 #Test your code here
+
+manager1 = Manager.new("brian", "arts", 100)
+manager2 = Manager.new("Chloe", "Science", 45)
+
+emp1 = Employee.new("Aaron", 19000, manager1)
+emp2 = Employee.new("Scott", 20000, manager2)
+emp3 = Employee.new("Ed", 100000, manager1)
+
 
 
 binding.pry
